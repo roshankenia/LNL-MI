cd1dfc3e8447cfffb2936a3dba815740d9dbe03a
Roshan Kenia <keniaroshan@gmail.com>
2022-06-30 14:18:07 -0400

    Update train.py

--- train.py
+++ train.py
@@ -1,13 +1,16 @@
 import torch
 import os
+import sys
 
 os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
 os.environ["CUDA_VISIBLE_DEVICES"] = "6"  # (xxxx is your specific GPU ID)
 
-
 print(torch.cuda.is_available())
 print(torch.cuda.device_count())
 print(torch.cuda.current_device())
+
+if not torch.cuda.is_available() or torch.cuda.device_count() != 1:
+    sys.exit()
 
 
 print('hi')
