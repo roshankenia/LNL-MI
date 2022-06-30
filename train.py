a3ca4bdc1ceac930241c295f120b8bae82fca065
Roshan Kenia <keniaroshan@gmail.com>
2022-06-30 14:23:25 -0400

    Update train.py

--- train.py
+++ train.py
@@ -1,14 +1,16 @@
 import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torchvision
+import torchvision.transforms as transforms
+import matplotlib.pyplot as plt
+import numpy as np
 import os
 import sys
 
-# os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
-# os.environ["CUDA_VISIBLE_DEVICES"] = "6"  # (xxxx is your specific GPU ID)
-
-print(torch.cuda.is_available())
-print(torch.cuda.device_count())
-print(torch.cuda.current_device())
-
+# ensure we are running on the correct gpu
+os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
+os.environ["CUDA_VISIBLE_DEVICES"] = "6"  # (xxxx is your specific GPU ID)
 if not torch.cuda.is_available() or torch.cuda.device_count() != 1:
     print('exiting')
     sys.exit()
