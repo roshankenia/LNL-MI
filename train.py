b9cdbdefd5e49b4d7f860c6440ef91ea48976b84
Roshan Kenia <keniaroshan@gmail.com>
2022-06-30 16:59:13 -0400

    Update train.py

--- train.py
+++ train.py
@@ -70,7 +70,7 @@ for epoch in range(num_epochs):
         loss.backward()
         optimizer.step()
 
-        if (i+1) % 2000 == 0:
+        if (i+1) % 100 == 0:
             print(
                 f'Epoch [{epoch+1}/{num_epochs}], Step [{i+1}/{n_total_steps}], Loss: {loss.item():.4f}')
 
@@ -91,7 +91,7 @@ with torch.no_grad():
         _, predicted = torch.max(outputs, 1)
         n_samples += labels.size(0)
         n_correct += (predicted == labels).sum().item()
-        print(labels.size)
+        print(labels)
         print(batch_size)
         for i in range(batch_size):
             label = labels[i]
