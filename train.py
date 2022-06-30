e2ed7e65b18a690e87acd414236c348bc084c310
Roshan Kenia <keniaroshan@gmail.com>
2022-06-30 14:15:29 -0400

    Update train.py

--- train.py
+++ train.py
@@ -8,3 +8,6 @@ os.environ["CUDA_VISIBLE_DEVICES"] = "6"  # (xxxx is your specific GPU ID)
 print(torch.cuda.is_available())
 print(torch.cuda.device_count())
 print(torch.cuda.current_device())
+
+
+print('hi')
