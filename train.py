6ea8a2e2f3569a2b3e969024c287ed69aa7ae9ac
Roshan Kenia <keniaroshan@gmail.com>
2022-07-01 22:30:07 -0400

    Update train.py

--- train.py
+++ train.py
@@ -8,6 +8,7 @@ import numpy as np
 import os
 import sys
 from ResNet34 import ResNet34
+from MI-Dataset import MedicalData
 
 # ensure we are running on the correct gpu
 os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
@@ -15,6 +16,8 @@ os.environ["CUDA_VISIBLE_DEVICES"] = "6"  # (xxxx is your specific GPU ID)
 if not torch.cuda.is_available() or torch.cuda.device_count() != 1:
     print('exiting')
     sys.exit()
+else:
+    print('GPU is being properly used')
 
 
 # Device configuration
@@ -22,35 +25,35 @@ device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 # Hyper-parameters
 num_epochs = 1
-batch_size = 128
+batch_size = 64
 learning_rate = 0.001
 
 # dataset has PILImage images of range [0, 1].
 # We transform them to Tensors of normalized range [-1, 1]
-transform = transforms.Compose(
-    [transforms.ToTensor(),
-     transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+# transform = transforms.Compose(
+#     [transforms.ToTensor(),
+#      transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
 
 # CIFAR10: 60000 32x32 color images in 10 classes, with 6000 images per class
-train_dataset = torchvision.datasets.CIFAR10(root='./data', train=True,
-                                             download=True, transform=transform)
+# train_dataset = torchvision.datasets.CIFAR10(root='./data', train=True,
+#                                              download=True, transform=transform)
 
-test_dataset = torchvision.datasets.CIFAR10(root='./data', train=False,
-                                            download=True, transform=transform)
-
-train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=batch_size,
+# test_dataset = torchvision.datasets.CIFAR10(root='./data', train=False,
+#                                             download=True, transform=transform)
+train_dataset = MedicalData()
+train_loader = torch.utils.data.DataLoader(dataset=train_dataset, batch_size=batch_size,
                                            shuffle=True)
 
-test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=batch_size,
-                                          shuffle=False)
+# test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=batch_size,
+#                                           shuffle=False)
 
 classes = ('plane', 'car', 'bird', 'cat',
            'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
 
 
-model = ResNet34(in_channels=3, outputs=10).to(device)
+model = ResNet34(in_channels=3, outputs=2).to(device)
 
-criterion = nn.CrossEntropyLoss()
+criterion = nn.BCELoss()
 optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate)
 
 n_total_steps = len(train_loader)
@@ -70,37 +73,37 @@ for epoch in range(num_epochs):
         loss.backward()
         optimizer.step()
 
-        if (i+1) % 100 == 0:
+        if (i+1) % 5 == 0:
             print(
                 f'Epoch [{epoch+1}/{num_epochs}], Step [{i+1}/{n_total_steps}], Loss: {loss.item():.4f}')
 
 print('Finished Training')
-# PATH = './cnn.pth'
-# torch.save(model.state_dict(), PATH)
+PATH = './resnet-mi.pth'
+torch.save(model.state_dict(), PATH)
 
-with torch.no_grad():
-    n_correct = 0
-    n_samples = 0
-    n_class_correct = [0 for i in range(10)]
-    n_class_samples = [0 for i in range(10)]
-    for images, labels in test_loader:
-        images = images.to(device)
-        labels = labels.to(device)
-        outputs = model(images)
-        # max returns (value ,index)
-        _, predicted = torch.max(outputs, 1)
-        n_samples += labels.size(0)
-        n_correct += (predicted == labels).sum().item()
-        for i in range(len(labels)):
-            label = labels[i]
-            pred = predicted[i]
-            if (label == pred):
-                n_class_correct[label] += 1
-            n_class_samples[label] += 1
+# with torch.no_grad():
+#     n_correct = 0
+#     n_samples = 0
+#     n_class_correct = [0 for i in range(10)]
+#     n_class_samples = [0 for i in range(10)]
+#     for images, labels in test_loader:
+#         images = images.to(device)
+#         labels = labels.to(device)
+#         outputs = model(images)
+#         # max returns (value ,index)
+#         _, predicted = torch.max(outputs, 1)
+#         n_samples += labels.size(0)
+#         n_correct += (predicted == labels).sum().item()
+#         for i in range(len(labels)):
+#             label = labels[i]
+#             pred = predicted[i]
+#             if (label == pred):
+#                 n_class_correct[label] += 1
+#             n_class_samples[label] += 1
 
-    acc = 100.0 * n_correct / n_samples
-    print(f'Accuracy of the network: {acc} %')
+#     acc = 100.0 * n_correct / n_samples
+#     print(f'Accuracy of the network: {acc} %')
 
-    for i in range(10):
-        acc = 100.0 * n_class_correct[i] / n_class_samples[i]
-        print(f'Accuracy of {classes[i]}: {acc} %')
+#     for i in range(10):
+#         acc = 100.0 * n_class_correct[i] / n_class_samples[i]
+#         print(f'Accuracy of {classes[i]}: {acc} %')
