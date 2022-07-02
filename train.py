35c4218489554eabd7be89c81647eeaccf841e85
Roshan Kenia <keniaroshan@gmail.com>
2022-07-02 14:47:54 -0400

    Update train.py

--- train.py
+++ train.py
@@ -27,7 +27,7 @@ device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 # Hyper-parameters
 num_epochs = 5
 batch_size = 64
-learning_rate = 0.001
+learning_rate = 0.01
 
 # dataset has PILImage images of range [0, 1].
 # We transform them to Tensors of normalized range [-1, 1]
