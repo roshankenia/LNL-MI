7e528eb56dbc83c14616f6a4d5398e05186963b4
Roshan Kenia <keniaroshan@gmail.com>
2022-07-03 22:56:26 -0400

    Update train.py

--- train.py
+++ train.py
@@ -26,7 +26,7 @@ device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 # Hyper-parameters
 num_epochs = 5
-batch_size = 512
+batch_size = 64
 learning_rate = 0.001
 
 # dataset has PILImage images of range [0, 1].
@@ -89,8 +89,8 @@ for epoch in range(num_epochs):
             print(
                 f'Epoch [{epoch+1}/{num_epochs}], Step [{i+1}/{n_total_steps}], Loss: {loss.item():.4f}')
             # print(binary_acc(outputs, labels))
-            # print(outputs)
-            # print(labels)
+            print(outputs)
+            print(labels)
 
 print('Finished Training')
 # PATH = './resnet-mi.pth'
