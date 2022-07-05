0970bcb86486b24854728eb6d42151c94fdc0d46
Roshan Kenia <keniaroshan@gmail.com>
2022-07-05 18:14:56 -0400

    Update cifar10-data-train.py

--- cifar10-data-train.py
+++ cifar10-data-train.py
@@ -34,8 +34,12 @@ labels = []
 i = 0
 while i < train_dataset.__len__():
     image, label = train_dataset[i]
-    images.append(transform(image).to(torch.float32))
-    labels.append(label)
+    if label == 4:
+        images.append(transform(image).to(torch.float32))
+        labels.append(0)
+    elif label == 7:
+        images.append(transform(image).to(torch.float32))
+        labels.append(1)
     if i % 1000 == 0:
         print('at', i)
     i += 1
