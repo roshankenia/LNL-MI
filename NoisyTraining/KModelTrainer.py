6748e63931bec52e0c754d3cd05182b5713ab06f
Roshan Kenia <keniaroshan@gmail.com>
2022-07-07 15:21:00 -0400

    Update KModelTrainer.py

--- NoisyTraining/KModelTrainer.py
+++ NoisyTraining/KModelTrainer.py
@@ -7,8 +7,8 @@ import matplotlib.pyplot as plt
 import numpy as np
 import os
 import sys
-from Cifar10TrainNoisy import Cifar10BinaryNoisy
-from Cifar10TestNoisy import Cifar10BinaryNoisyTest
+from DataSplitter import KDataSplitter
+from ModelTrain import FDModel
 
 # ensure we are running on the correct gpu
 os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
@@ -18,3 +18,24 @@ if not torch.cuda.is_available() or torch.cuda.device_count() != 1:
     sys.exit()
 else:
     print('GPU is being properly used')
+
+
+class KModelTrain():
+
+    def __init__(self, x, y, k):
+        self.data_splitter = KDataSplitter(x, y, k)
+        x_arrays, y_arrays = self.data_splitter.split()
+
+        self.x_arrays = x_arrays
+        self.y_arrays = y_arrays
+        # set up models
+        self.models = []
+        for i in range(k):
+            # create kth model
+            newModel = FDModel(
+                self.x_arrays[i], self.y_arrays[i], num_epochs=5, batch_size=64, learning_rate=0.001)
+            # train new model
+            newModel.train()
+
+            # add to models
+            self.models.append(newModel)
