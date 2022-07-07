1f6ab069b046eefa37fcd866b5314cc9584379a8
Roshan Kenia <keniaroshan@gmail.com>
2022-07-07 15:25:25 -0400

    Update FurthestDistanceNoisy.py

--- NoisyTraining/FurthestDistanceNoisy.py
+++ NoisyTraining/FurthestDistanceNoisy.py
@@ -7,8 +7,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import os
 import sys
-from Cifar10TrainNoisy import Cifar10BinaryNoisy
-from Cifar10TestNoisy import Cifar10BinaryNoisyTest
+from KModelTrainer import KModelTrain
 
 # ensure we are running on the correct gpu
 os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
@@ -19,83 +18,92 @@ if not torch.cuda.is_available() or torch.cuda.device_count() != 1:
 else:
     print('GPU is being properly used')
 
+# obtain data tensors
 
-# Device configuration
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+x_tensor = torch.load('cifar10_noisy_data_tensor_nonorm.pt')
+# size [n_samples, 1]
+y_tensor = torch.load('cifar10_noisy_ground_truth_tensor_nonorm.pt')
 
-# Hyper-parameters
-num_epochs = 50
-batch_size = 512
-learning_rate = 0.01
+# make our K Model Trainer where k represents number of models
+model_trainer = KModelTrain(x_tensor, y_tensor, k=4)
 
-# dataset has PILImage images of range [0, 1].
-# We transform them to Tensors of normalized range [-1, 1]
 
-train_dataset = Cifar10BinaryNoisy()
-train_loader = torch.utils.data.DataLoader(dataset=train_dataset, batch_size=batch_size,
-                                           shuffle=True)
+# # Device configuration
+# device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-test_dataset = Cifar10BinaryNoisyTest()
-test_loader = torch.utils.data.DataLoader(dataset=test_dataset, batch_size=batch_size,
-                                          shuffle=False)
+# # Hyper-parameters
+# num_epochs = 50
+# batch_size = 512
+# learning_rate = 0.01
 
-model = torchvision.models.resnet34(pretrained=False, num_classes=1).to(device)
+# # dataset has PILImage images of range [0, 1].
+# # We transform them to Tensors of normalized range [-1, 1]
 
-criterion = nn.BCEWithLogitsLoss()
-optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate)
-# scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=15, gamma=0.1)
+# train_dataset = Cifar10BinaryNoisy()
+# train_loader = torch.utils.data.DataLoader(dataset=train_dataset, batch_size=batch_size,
+#                                            shuffle=True)
 
-n_total_steps = len(train_loader)
-for epoch in range(num_epochs):
-    for i, (images, labels) in enumerate(train_loader):
-        # origin shape: [4, 3, 32, 32] = 4, 3, 1024
-        images = images.to(device)
-        labels = labels.to(device)
+# test_dataset = Cifar10BinaryNoisyTest()
+# test_loader = torch.utils.data.DataLoader(dataset=test_dataset, batch_size=batch_size,
+#                                           shuffle=False)
 
-        # Forward pass
-        outputs = model(images)
-        loss = criterion(outputs, labels)
+# model = torchvision.models.resnet34(pretrained=False, num_classes=1).to(device)
 
-        # Backward and optimize
-        optimizer.zero_grad()
-        loss.backward()
-        optimizer.step()
-        # scheduler.step()
+# criterion = nn.BCEWithLogitsLoss()
+# optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate)
+# # scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=15, gamma=0.1)
 
-        if (i+1) % 10 == 0:
-            print(
-                f'Epoch [{epoch+1}/{num_epochs}], Step [{i+1}/{n_total_steps}], Loss: {loss.item():.4f}')
-            # print(binary_acc(outputs, labels))
-            # print(outputs)
-            # print(labels)
+# n_total_steps = len(train_loader)
+# for epoch in range(num_epochs):
+#     for i, (images, labels) in enumerate(train_loader):
+#         # origin shape: [4, 3, 32, 32] = 4, 3, 1024
+#         images = images.to(device)
+#         labels = labels.to(device)
 
-print('Finished Training')
-# PATH = './resnet-mi.pth'
-# torch.save(model.state_dict(), PATH)
+#         # Forward pass
+#         outputs = model(images)
+#         loss = criterion(outputs, labels)
 
-with torch.no_grad():
-    n_correct = 0
-    n_samples = 0
-    x = 0
-    for images, labels in test_loader:
-        images = images.to(device)
-        labels = labels.to(device)
-        outputs = model(images)
+#         # Backward and optimize
+#         optimizer.zero_grad()
+#         loss.backward()
+#         optimizer.step()
+#         # scheduler.step()
 
-        y_test_pred = torch.sigmoid(outputs)
-        y_pred_tag = torch.round(y_test_pred)
+#         if (i+1) % 10 == 0:
+#             print(
+#                 f'Epoch [{epoch+1}/{num_epochs}], Step [{i+1}/{n_total_steps}], Loss: {loss.item():.4f}')
+#             # print(binary_acc(outputs, labels))
+#             # print(outputs)
+#             # print(labels)
 
-        if x == 0:
-            # print('labels:', labels[0:20])
-            # print('outputs:', outputs[0:20])
-            # print('predicted:', y_pred_tag[0:20])
-            x = 1
+# print('Finished Training')
+# # PATH = './resnet-mi.pth'
+# # torch.save(model.state_dict(), PATH)
 
-        n_correct += (y_pred_tag == labels).sum().float()
-        n_samples += len(labels)
-    acc = n_correct/n_samples
-    acc = torch.round(acc * 100)
-    print('Number correct:', n_correct.item(), 'out of:', n_samples)
-    noise_index_tensor = torch.load('cifar10_noisy_index_tensor.pt')
-    print('Number of noisy samples:', len(noise_index_tensor))
-    print(f'Accuracy of the network: {acc} %')
+# with torch.no_grad():
+#     n_correct = 0
+#     n_samples = 0
+#     x = 0
+#     for images, labels in test_loader:
+#         images = images.to(device)
+#         labels = labels.to(device)
+#         outputs = model(images)
+
+#         y_test_pred = torch.sigmoid(outputs)
+#         y_pred_tag = torch.round(y_test_pred)
+
+#         if x == 0:
+#             # print('labels:', labels[0:20])
+#             # print('outputs:', outputs[0:20])
+#             # print('predicted:', y_pred_tag[0:20])
+#             x = 1
+
+#         n_correct += (y_pred_tag == labels).sum().float()
+#         n_samples += len(labels)
+#     acc = n_correct/n_samples
+#     acc = torch.round(acc * 100)
+#     print('Number correct:', n_correct.item(), 'out of:', n_samples)
+#     noise_index_tensor = torch.load('cifar10_noisy_index_tensor.pt')
+#     print('Number of noisy samples:', len(noise_index_tensor))
+#     print(f'Accuracy of the network: {acc} %')
