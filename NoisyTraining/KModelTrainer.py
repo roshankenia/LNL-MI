726ba66d25917bbd37f1862e0a026568e48a1932
Roshan Kenia <keniaroshan@gmail.com>
2022-07-08 15:35:06 -0400

    Update KModelTrainer.py

--- NoisyTraining/KModelTrainer.py
+++ NoisyTraining/KModelTrainer.py
@@ -37,7 +37,7 @@ class KModelTrain():
             x_data = torch.cat(
                 (self.x_arrays[i], self.x_arrays[nextIndex]), dim=0)
             y_data = torch.cat(
-                (self.x_arrays[i], self.x_arrays[nextIndex]), dim=0)
+                (self.y_arrays[i], self.y_arrays[nextIndex]), dim=0)
             # create kth model
             newModel = FDModel(
                 x_data, y_data, num_epochs=50, batch_size=64, learning_rate=0.01)
