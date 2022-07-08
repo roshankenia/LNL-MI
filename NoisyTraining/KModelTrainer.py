11bc2df3a02191ca33918a47ae64b58818210022
Roshan Kenia <keniaroshan@gmail.com>
2022-07-08 13:04:59 -0400

    Update KModelTrainer.py

--- NoisyTraining/KModelTrainer.py
+++ NoisyTraining/KModelTrainer.py
@@ -55,16 +55,20 @@ class KModelTrain():
                 # obtain predictions from each model
                 predictions = []
                 for model in self.models:
-                    predictions.append(torch.sigmoid(model.predict(x_sample)).item())
-                    
+                    predictions.append(torch.sigmoid(
+                        model.predict(x_sample)).item())
+
                 print(predictions)
                 predictions = torch.tensor(predictions)
                 # calculate average probability
                 y_avg = torch.mean(predictions)
-                print(y_avg.item())
+                y_avg = torch.unsqueeze(y_avg, 0)
+                print(y_avg)
 
                 # compute binary cross entropy loss using this average
-                bce = loss(y_avg.item(), y_sample)
+                bce = loss(y_avg, y_sample)
+
+                print('bce:', bce)
 
                 # now we need to compute the furthest apart metric
                 distances = []
