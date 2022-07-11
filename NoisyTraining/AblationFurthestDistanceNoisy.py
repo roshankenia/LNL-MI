6f170afa1d7984eb2b906cc17517120e7a8865f5
Roshan Kenia <keniaroshan@gmail.com>
2022-07-11 16:59:59 -0400

    Raise epochs

--- NoisyTraining/AblationFurthestDistanceNoisy.py
+++ NoisyTraining/AblationFurthestDistanceNoisy.py
@@ -37,7 +37,7 @@ for index in noise_indexes:
     noisy_data[int(index.item())] = 1
 
 
-for i in range(20):
+for i in range(5):
     # make our K Model Trainer where k represents number of models
     model_trainer = KModelTrain(x_tensor, y_tensor, k=8)
 
