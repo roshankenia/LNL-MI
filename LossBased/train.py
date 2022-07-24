15e1d8a5899808afef8da67f4f5ae471eea9fe0d
Roshan Kenia <keniaroshan@gmail.com>
2022-07-24 19:07:33 -0400

    Update train.py

--- LossBased/train.py
+++ LossBased/train.py
@@ -57,10 +57,10 @@ def train(train_loader, epoch, fullModel, fullOptimizer, epochs, train_len, batc
             fullLoss, relabelCount = cross_entropy_loss_update(
                 logits1, labels, epochLabels, ind)
         else:
-            # fullLoss, purity_ratio_clean, purity_ratio_noisy, num_clean, num_noisy, relabelCount = low_loss_over_epochs_labels(
-            #     logits1, labels, epochLabels, ind)
-            fullLoss, purity_ratio_clean, purity_ratio_noisy, num_clean, num_noisy, relabelCount = no_split(
+            fullLoss, purity_ratio_clean, purity_ratio_noisy, num_clean, num_noisy, relabelCount = low_loss_over_epochs_labels(
                 logits1, labels, epochLabels, ind)
+            # fullLoss, purity_ratio_clean, purity_ratio_noisy, num_clean, num_noisy, relabelCount = no_split(
+            #     logits1, labels, epochLabels, ind)
 
         totalRelabelCount += relabelCount[0]
         totalCorrectRelabelCount += relabelCount[1]
