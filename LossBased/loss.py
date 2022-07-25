327dbefa07950e046bf5edd82c6fa694882ad8df
Roshan Kenia <keniaroshan@gmail.com>
2022-07-24 21:34:05 -0400

    Update loss.py

--- LossBased/loss.py
+++ LossBased/loss.py
@@ -27,6 +27,9 @@ def low_loss_over_epochs_labels(y_1, t, lowest_loss, indices):
     peakValues = []
     for i in range(len(y_1)):
         predictions = torch.sort(y_1[i].clone().detach())
+        print(y_1[i])
+        print(predictions)
+        print(predictions[1:])
         peakValues.append(predictions[0]/torch.sum(predictions[1:]))
     print(peakValues)
 
