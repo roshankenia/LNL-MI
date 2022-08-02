ae01275d0cf2927df383926aca6c2782b9b69b9f
Roshan Kenia <keniaroshan@gmail.com>
2022-08-01 22:56:05 -0400

    Update combinedLoss.py

--- LossBased/combinedLoss.py
+++ LossBased/combinedLoss.py
@@ -48,11 +48,8 @@ def loss_coteaching_with_relabeling(y_1, y_2, t, indices, combinedLabels, cur_ti
     loss_2_sorted = loss_2[ind_2_sorted]
 
     # find number of samples to use
-    remember_rate = 1 - 0.5
-    num_use = int(remember_rate * len(loss_1_sorted))
-
-    # num_use = torch.nonzero(combined_cross_entropy_loss <
-    #                         combined_cross_entropy_loss.mean()).shape[0]
+    num_use = torch.nonzero(combined_cross_entropy_loss <
+                            combined_cross_entropy_loss.mean()).shape[0]
 
     pure_ratio_1 = np.sum(
         noise_or_not[ind_1_sorted[:num_use]].numpy())/float(num_use)
@@ -99,11 +96,8 @@ def loss_coteaching_with_no_relabeling(y_1, y_2, t, indices, combinedLabels, cur
 
     # find number of samples to use
 
-    remember_rate = 1 - 0.5
-    num_use = int(remember_rate * len(loss_1_sorted))
-
-    # num_use = torch.nonzero(combined_cross_entropy_loss <
-    #                         combined_cross_entropy_loss.mean()).shape[0]
+    num_use = torch.nonzero(combined_cross_entropy_loss <
+                            combined_cross_entropy_loss.mean()).shape[0]
 
     pure_ratio_1 = np.sum(
         noise_or_not[ind_1_sorted[:num_use]].numpy())/float(num_use)
