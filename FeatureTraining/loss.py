ca143aaed3d069db757ff7719f872df6cd894166
Roshan Kenia <keniaroshan@gmail.com>
2022-08-17 14:05:51 -0400

    Feature Vector Training

---
+++ FeatureTraining/loss.py
@@ -0,0 +1,55 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.autograd import Variable
+import numpy as np
+import os
+import sys
+from utils.labels import LowLossLabels
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+# ensure we are running on the correct gpu
+os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
+os.environ["CUDA_VISIBLE_DEVICES"] = "6"  # (xxxx is your specific GPU ID)
+if not torch.cuda.is_available() or torch.cuda.device_count() != 1:
+    print('exiting')
+    sys.exit()
+else:
+    print('GPU is being properly used')
+
+# Loss functions
+
+
+def loss_coteaching(y_1, y_2, t, noise_or_not):
+    loss_1 = F.cross_entropy(y_1, t, reduce=False)
+    ind_1_sorted = np.argsort(loss_1.data.cpu())
+    loss_1_sorted = loss_1[ind_1_sorted]
+
+    loss_2 = F.cross_entropy(y_2, t, reduce=False)
+    ind_2_sorted = np.argsort(loss_2.data.cpu())
+    loss_2_sorted = loss_2[ind_2_sorted]
+
+    # find number of samples to use
+    # num_use_1 = torch.nonzero(loss_1 < loss_1.mean()).shape[0]
+    # num_use_2 = torch.nonzero(loss_2 < loss_2.mean()).shape[0]
+    remember_rate = 1 - 0.5
+    num_remember = int(remember_rate * len(loss_1_sorted))
+
+    pure_ratio_1 = np.sum(
+        noise_or_not[ind_1_sorted[:num_remember]].numpy())/float(num_remember)
+    pure_ratio_2 = np.sum(
+        noise_or_not[ind_2_sorted[:num_remember]].numpy())/float(num_remember)
+
+    ind_1_update = ind_1_sorted[:num_remember]
+    ind_2_update = ind_2_sorted[:num_remember]
+    # exchange
+    loss_1_update = F.cross_entropy(
+        y_1[ind_2_update], t[ind_2_update])
+    # print('before:', loss_1_update)
+    # print('after:', torch.sum(loss_1_update)/num_remember)
+    loss_2_update = F.cross_entropy(
+        y_2[ind_1_update], t[ind_1_update])
+
+    return torch.sum(loss_1_update)/num_remember, torch.sum(loss_2_update)/num_remember, pure_ratio_1, pure_ratio_2
---
+++ FeatureTraining/train.py
@@ -0,0 +1,93 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torchvision
+import torchvision.transforms as transforms
+from torch.autograd import Variable
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import os
+import sys
+import time
+import argparse
+from data.cifar import CIFAR10, CIFAR100
+from loss import loss_coteaching
+
+# ensure we are running on the correct gpu
+os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
+os.environ["CUDA_VISIBLE_DEVICES"] = "6"  # (xxxx is your specific GPU ID)
+if not torch.cuda.is_available() or torch.cuda.device_count() != 1:
+    print('exiting')
+    sys.exit()
+else:
+    print('GPU is being properly used')
+
+
+def train(train_loader, epoch, model_1, optimizer_1, model_2, optimizer_2, epochs, train_len, batch_size, noise_or_not):
+    train_total = 0
+    train_correct = 0
+    pure_ratio_1_list = []
+    pure_ratio_2_list = []
+    for i, (images, labels, indexes) in enumerate(train_loader):
+        ind = indexes.cpu().numpy().transpose()
+        noise = noise_or_not[ind]
+        # if i > args.num_iter_per_epoch:
+        #     break
+
+        images = Variable(images).cuda()
+        cur_labels = Variable(cur_labels).cuda()
+
+        # Forward + Backward + Optimize
+        logits_1 = model_1(images)
+        logits_2 = model_2(images)
+        combinedLogits = (logits_1+logits_2)/2
+        prec, _ = accuracy(combinedLogits, cur_labels, topk=(1, 5))
+        prec1, _ = accuracy(logits_1, cur_labels, topk=(1, 5))
+        prec2, _ = accuracy(logits_2, cur_labels, topk=(1, 5))
+        train_total += 1
+        train_correct += prec
+
+        # calculate loss
+        loss_1 = None
+        loss_2 = None
+        pure_ratio_1 = None
+        pure_ratio_2 = None
+
+        loss_1, loss_2, pure_ratio_1, pure_ratio_2 = loss_coteaching(
+            logits_1, logits_2, cur_labels, noise)
+
+        pure_ratio_1_list.append(100*pure_ratio_1)
+        pure_ratio_2_list.append(100*pure_ratio_2)
+
+        optimizer_1.zero_grad()
+        loss_1.backward()
+        optimizer_1.step()
+
+        optimizer_2.zero_grad()
+        loss_2.backward()
+        optimizer_2.step()
+
+        if (i+1) % 50 == 0:
+            print('Epoch [%d/%d], Iter [%d/%d], Combined Accuracy: %.4F, Training Accuracy1: %.4F, Training Accuracy2: %.4F, Loss1: %.4f, Loss2: %.4f, Pure Ratio1: %.4f, Pure Ratio2 %.4f'
+                  % (epoch+1, epochs, i+1, train_len//batch_size, prec.data.item(), prec1, prec2, loss_1.data.item(), loss_2.data.item(), np.sum(pure_ratio_1_list)/len(pure_ratio_1_list), np.sum(pure_ratio_2_list)/len(pure_ratio_2_list)))
+    train_acc1 = float(train_correct)/float(train_total)
+    return train_acc1
+
+
+def accuracy(logit, target, topk=(1,)):
+    """Computes the precision@k for the specified values of k"""
+    output = F.softmax(logit, dim=1)
+    maxk = max(topk)
+    batch_size = target.size(0)
+
+    _, pred = output.topk(maxk, 1, True, True)
+    pred = pred.t()
+    correct = pred.eq(target.view(1, -1).expand_as(pred))
+
+    res = []
+    for k in topk:
+        correct_k = correct[:k].reshape(-1).float().sum(0, keepdim=True)
+        res.append(correct_k.mul_(100.0 / batch_size))
+    return res
