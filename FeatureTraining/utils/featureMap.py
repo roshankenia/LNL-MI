39c9e3e3f0c181f770c8e0e31bd0696655dba5bf
Roshan Kenia <keniaroshan@gmail.com>
2022-08-20 22:20:13 -0400

    Update featureMap.py

--- FeatureTraining/utils/featureMap.py
+++ FeatureTraining/utils/featureMap.py
@@ -6,6 +6,13 @@ from torch.distributions import Categorical
 import numpy as np
 import os
 import sys
+import seaborn as sns
+import pandas as pd
+from sklearn.manifold import TSNE
+import numpy as np
+import random
+import matplotlib.pyplot as plt
+import os
 
 # ensure we are running on the correct gpu
 os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
@@ -37,3 +44,40 @@ class FeatureMap():
             probs = torch.sort(F.softmax(sampleLogits, dim=0)).values
             time = epoch % 10
             self.features[index][time] = probs
+
+    def makePlot(self, epoch, labels, noise):
+        # We want to get TSNE embedding with 2 dimensions
+        n_components = 2
+        tsne = TSNE(n_components)
+        tsne_result = tsne.fit_transform(self.features)
+        tsne_result.shape
+        # Two dimensions for each of our images
+        # Plot the result of our TSNE with the label color coded
+        # A lot of the stuff here is about making the plot look pretty and not TSNE
+        tsne_result_df = pd.DataFrame(
+            {'tSNE Feature 1': tsne_result[:, 0], 'tSNE Feature 2': tsne_result[:, 1], 'label': labels, 'noise': noise})
+        fig, ax = plt.subplots(figsize=(10, 10))
+        sns.scatterplot(x='tSNE Feature 1', y='tSNE Feature 2',
+                        hue='label', data=tsne_result_df, ax=ax, s=10)
+        lim = (tsne_result.min()-5, tsne_result.max()+5)
+        plt.title('Sample Predictions over last 10 epochs reduced')
+        ax.set_xlim(lim)
+        ax.set_ylim(lim)
+        ax.set_aspect('equal')
+        ax.legend(bbox_to_anchor=(1.05, 1), loc=2, borderaxespad=0.0)
+        plot_title = 'tSNE-Labels-'+epoch+'.png'
+        plt.savefig(plot_title)
+        plt.close()
+
+        fig, ax = plt.subplots(figsize=(10, 10))
+        sns.scatterplot(x='tSNE Feature 1', y='tSNE Feature 2',
+                        hue='noise', data=tsne_result_df, ax=ax, s=10)
+        lim = (tsne_result.min()-5, tsne_result.max()+5)
+        plt.title('Sample Predictions over last 10 epochs reduced')
+        ax.set_xlim(lim)
+        ax.set_ylim(lim)
+        ax.set_aspect('equal')
+        ax.legend(bbox_to_anchor=(1.05, 1), loc=2, borderaxespad=0.0)
+        plot_title = 'tSNE-Noise-'+epoch+'.png'
+        plt.savefig(plot_title)
+        plt.close()
