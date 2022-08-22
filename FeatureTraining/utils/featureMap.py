fe795e21e906119da873186625bb05f227329901
Roshan Kenia <keniaroshan@gmail.com>
2022-08-22 12:17:31 -0400

    Update featureMap.py

--- FeatureTraining/utils/featureMap.py
+++ FeatureTraining/utils/featureMap.py
@@ -9,6 +9,7 @@ import sys
 import seaborn as sns
 import pandas as pd
 from sklearn.manifold import TSNE
+from sklearn.decomposition import PCA
 import numpy as np
 import random
 import matplotlib.pyplot as plt
@@ -53,38 +54,74 @@ class FeatureMap():
         reshapeSize = self.history*self.num_classes
         reshapeFeatures = torch.reshape(
             self.features, (self.num_samples, reshapeSize))
-        # We want to get TSNE embedding with 2 dimensions
+        # # We want to get TSNE embedding with 2 dimensions
+        # n_components = 2
+        # tsne = TSNE(n_components)
+        # tsne_result = tsne.fit_transform(reshapeFeatures)
+        # tsne_result.shape
+        # # Two dimensions for each of our images
+        # # Plot the result of our TSNE with the label color coded
+        # # A lot of the stuff here is about making the plot look pretty and not TSNE
+        # tsne_result_df = pd.DataFrame(
+        #     {'tSNE Feature 1': tsne_result[:, 0], 'tSNE Feature 2': tsne_result[:, 1], 'label': labels, 'noise': noise})
+        # fig, ax = plt.subplots(figsize=(10, 10))
+        # sns.scatterplot(x='tSNE Feature 1', y='tSNE Feature 2',
+        #                 hue='label', data=tsne_result_df, ax=ax, s=10)
+        # lim = (tsne_result.min()-5, tsne_result.max()+5)
+        # plt.title('tSNE Sample Predictions over last 10 epochs reduced')
+        # ax.set_xlim(lim)
+        # ax.set_ylim(lim)
+        # ax.set_aspect('equal')
+        # ax.legend(bbox_to_anchor=(1.05, 1), loc=2, borderaxespad=0.0)
+        # plot_title = 'tSNE-Labels-'+str(epoch+1)+'.png'
+        # plt.savefig(plot_title)
+        # plt.close()
+
+        # fig, ax = plt.subplots(figsize=(10, 10))
+        # sns.scatterplot(x='tSNE Feature 1', y='tSNE Feature 2',
+        #                 hue='noise', data=tsne_result_df, ax=ax, s=10)
+        # lim = (tsne_result.min()-5, tsne_result.max()+5)
+        # plt.title('tSNE Sample Predictions over last 10 epochs reduced')
+        # ax.set_xlim(lim)
+        # ax.set_ylim(lim)
+        # ax.set_aspect('equal')
+        # ax.legend(bbox_to_anchor=(1.05, 1), loc=2, borderaxespad=0.0)
+        # plot_title = 'tSNE-Noise-'+str(epoch+1)+'.png'
+        # plt.savefig(plot_title)
+        # plt.close()
+
+        # We want to get PCA embedding with 2 dimensions
         n_components = 2
-        tsne = TSNE(n_components)
-        tsne_result = tsne.fit_transform(reshapeFeatures)
-        tsne_result.shape
+        pca = PCA(n_components)
+        pca_result = pca.fit_transform(reshapeFeatures)
+        pca_result.shape
         # Two dimensions for each of our images
         # Plot the result of our TSNE with the label color coded
         # A lot of the stuff here is about making the plot look pretty and not TSNE
-        tsne_result_df = pd.DataFrame(
-            {'tSNE Feature 1': tsne_result[:, 0], 'tSNE Feature 2': tsne_result[:, 1], 'label': labels, 'noise': noise})
+        pca_result_df = pd.DataFrame(
+            {'PCA Feature 1': pca_result[:, 0], 'PCA Feature 2': pca_result[:, 1], 'label': labels, 'noise': noise})
         fig, ax = plt.subplots(figsize=(10, 10))
-        sns.scatterplot(x='tSNE Feature 1', y='tSNE Feature 2',
-                        hue='label', data=tsne_result_df, ax=ax, s=10)
-        lim = (tsne_result.min()-5, tsne_result.max()+5)
-        plt.title('Sample Predictions over last 10 epochs reduced')
+        sns.scatterplot(x='PCA Feature 1', y='PCA Feature 2',
+                        hue='label', data=pca_result_df, ax=ax, s=10)
+        lim = (pca_result.min()-5, pca_result.max()+5)
+        plt.title('PCA Sample Predictions over last 10 epochs reduced')
         ax.set_xlim(lim)
         ax.set_ylim(lim)
         ax.set_aspect('equal')
         ax.legend(bbox_to_anchor=(1.05, 1), loc=2, borderaxespad=0.0)
-        plot_title = 'tSNE-Labels-'+str(epoch+1)+'.png'
+        plot_title = 'pca-Labels-'+str(epoch+1)+'.png'
         plt.savefig(plot_title)
         plt.close()
 
         fig, ax = plt.subplots(figsize=(10, 10))
-        sns.scatterplot(x='tSNE Feature 1', y='tSNE Feature 2',
-                        hue='noise', data=tsne_result_df, ax=ax, s=10)
-        lim = (tsne_result.min()-5, tsne_result.max()+5)
-        plt.title('Sample Predictions over last 10 epochs reduced')
+        sns.scatterplot(x='PCA Feature 1', y='PCA Feature 2',
+                        hue='noise', data=pca_result_df, ax=ax, s=10)
+        lim = (pca_result.min()-5, pca_result.max()+5)
+        plt.title('PCA Sample Predictions over last 10 epochs reduced')
         ax.set_xlim(lim)
         ax.set_ylim(lim)
         ax.set_aspect('equal')
         ax.legend(bbox_to_anchor=(1.05, 1), loc=2, borderaxespad=0.0)
-        plot_title = 'tSNE-Noise-'+str(epoch+1)+'.png'
+        plot_title = 'pca-Noise-'+str(epoch+1)+'.png'
         plt.savefig(plot_title)
         plt.close()
