a0228871423ea87b8b0e35c85f19ad64248acd01
Roshan Kenia <keniaroshan@gmail.com>
2022-08-26 13:24:10 -0400

    Update extraction.py

--- FeatureTraining/extraction/extraction.py
+++ FeatureTraining/extraction/extraction.py
@@ -47,6 +47,7 @@ def extract_features(x_data):
 def make_plots(features, labels, noise_or_not, num_classes):
     for label in range(num_classes):
         # find all indexes with this label
+        print(labels)
         indexes = np.where(labels == [label])
         print(indexes)
         currentFeatures = features[indexes].detach().numpy()
