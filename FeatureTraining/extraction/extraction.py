7cce7a4e152c0a48bae91952dafbfb832ec8a7a1
Roshan Kenia <keniaroshan@gmail.com>
2022-08-27 17:50:06 -0400

    Update extraction.py

--- FeatureTraining/extraction/extraction.py
+++ FeatureTraining/extraction/extraction.py
@@ -54,23 +54,39 @@ def make_plots(features, labels, noise_or_not, num_classes):
         print(currentFeatures.shape)
 
         # run tSNE on the current features
-        n_components = 2
+        n_components = 3
         tsne = TSNE(n_components)
         tsne_result = tsne.fit_transform(currentFeatures)
         tsne_result.shape
 
         tsne_result_df = pd.DataFrame(
-            {'tSNE Feature 1': tsne_result[:, 0], 'tSNE Feature 2': tsne_result[:, 1], 'noise': noise_or_not[indexes]})
+            {'tSNE Feature 1': tsne_result[:, 0], 'tSNE Feature 2': tsne_result[:, 1], 'tSNE Feature 3': tsne_result[:, 2], 'noise': noise_or_not[indexes]})
 
-        fig, ax = plt.subplots(figsize=(10, 10))
-        sns.scatterplot(x='tSNE Feature 1', y='tSNE Feature 2',
-                        hue='noise', data=tsne_result_df, ax=ax, s=10)
+        # fig, ax = plt.subplots(figsize=(10, 10))
+        # sns.scatterplot(x='tSNE Feature 1', y='tSNE Feature 2',
+        #                 hue='noise', data=tsne_result_df, ax=ax, s=10)
+        # lim = (tsne_result.min()-5, tsne_result.max()+5)
+        # plt.title('tSNE for Features')
+        # ax.set_xlim(lim)
+        # ax.set_ylim(lim)
+        # ax.set_aspect('equal')
+        # ax.legend(bbox_to_anchor=(1.05, 1), loc=2, borderaxespad=0.0)
+        # plot_title = 'tSNE-Features-'+str(label)+'.png'
+        # plt.savefig(plot_title)
+        # plt.close()
+
+        fig = plt.figure(figsize=(12, 12))
+        ax = fig.add_subplot(projection='3d')
+
+        ax.scatter(tsne_result_df['tSNE Feature 1'], tsne_result_df['tSNE Feature 2'],
+                   tsne_result_df['tSNE Feature 3'], c=tsne_result_df['noise'])
         lim = (tsne_result.min()-5, tsne_result.max()+5)
         plt.title('tSNE for Features')
         ax.set_xlim(lim)
         ax.set_ylim(lim)
+        ax.set_zlim(lim)
         ax.set_aspect('equal')
         ax.legend(bbox_to_anchor=(1.05, 1), loc=2, borderaxespad=0.0)
-        plot_title = 'tSNE-Features-'+str(label)+'.png'
+        plot_title = 'tSNE-Features-3D-'+str(label)+'.png'
         plt.savefig(plot_title)
         plt.close()
